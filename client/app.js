e00c13baacf246cfe101a372d3771257b148726d
Plngchil <jakubgalazka34@gmail.com>
2025-02-20 12:46:05 +0100

    commit

---
+++ client/app.js
@@ -0,0 +1,86 @@
+let table = document.querySelector("table")
+let btnWszystkie = document.getElementById("wszystkie")
+let btnDostepne = document.getElementById("dostepne")
+
+async function getSamochody(){
+    let data = await fetch('http://localhost:3000/samochody')
+    let tdata = await data.json()
+    console.log(tdata);
+    table.innerHTML = `
+    <tr>
+        <th>Identyfikator</th>
+            <th>Marka</th>
+            <th>Model</th>
+            <th>Rok produkcji</th>
+            <th>Cena za dobe</th>
+            <th>Dostępny</th> 
+    </tr>`
+    for(let i = 0; i<tdata.length; i++){
+        let tr = document.createElement("tr")
+        let idtd = document.createElement("td")
+        let markatd = document.createElement("td")
+        let modeltd = document.createElement("td")
+        let roktd = document.createElement("td")
+        let cenatd = document.createElement("td")
+        let dostepnosctd = document.createElement("td")
+        tr.appendChild(idtd)
+        tr.appendChild(markatd)
+        tr.appendChild(modeltd)
+        tr.appendChild(roktd)
+        tr.appendChild(cenatd)
+        tr.appendChild(dostepnosctd)
+        idtd.innerHTML = tdata[i].id
+        markatd.innerHTML = tdata[i].marka
+        modeltd.innerHTML = tdata[i].model
+        roktd.innerHTML = tdata[i].rok_produkcji
+        cenatd.innerHTML = tdata[i].cena_za_dobe
+        dostepnosctd.innerHTML = tdata[i].czy_dostepny
+        table.appendChild(tr)
+        btnWszystkie.disabled = "true"
+    }
+}
+async function getDosSamochody(){
+        let data = await fetch('http://localhost:3000/dossamochody')
+        let tdata = await data.json()
+        console.log(tdata);
+        table.innerHTML = `
+    <tr>
+        <th>Identyfikator</th>
+            <th>Marka</th>
+            <th>Model</th>
+            <th>Rok produkcji</th>
+            <th>Cena za dobe</th>
+            <th>Dostępny</th> 
+    </tr>`
+        for(let i = 0; i<tdata.length; i++){
+            let tr = document.createElement("tr")
+            let idtd = document.createElement("td")
+            let markatd = document.createElement("td")
+            let modeltd = document.createElement("td")
+            let roktd = document.createElement("td")
+            let cenatd = document.createElement("td")
+            let dostepnosctd = document.createElement("td")
+            tr.appendChild(idtd)
+            tr.appendChild(markatd)
+            tr.appendChild(modeltd)
+            tr.appendChild(roktd)
+            tr.appendChild(cenatd)
+            tr.appendChild(dostepnosctd)
+            idtd.innerHTML = tdata[i].id
+            markatd.innerHTML = tdata[i].marka
+            modeltd.innerHTML = tdata[i].model
+            roktd.innerHTML = tdata[i].rok_produkcji
+            cenatd.innerHTML = tdata[i].cena_za_dobe
+            dostepnosctd.innerHTML = tdata[i].czy_dostepny
+            table.appendChild(tr)
+            btnDostepne.disabled = "true"
+            btnWszystkie.disabled = "false"
+}
+}
+btnWszystkie.addEventListener("click",async ()=>{
+    await getSamochody()
+})
+btnDostepne.addEventListener("click",async ()=>{
+    await getDosSamochody()
+})
+getSamochody()
---
+++ serwer/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const cors = require("cors")
+const mysql = require("mysql")
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "wypozyczalnia"
+})
+
+const app=express()
+app.use(cors())
+
+con.connect((err)=>{
+    if(!err){
+        console.log("połączono z bazą");
+    }else{
+        console.log("błąd połączenia z bazą" + err);
+    }        
+app.get("/samochody", (req,res)=>{
+    const sql = "select * from samochody"
+    con.query(sql,(err,wynik,info_wynik)=>{
+        res.send(wynik)
+    })
+})
+app.get("/dossamochody", (req,res)=>{
+    const sql = "select * from samochody where czy_dostepny = 'TAK'"
+    con.query(sql,(err,wynik,info_wynik)=>{
+        res.send(wynik)
+    })
+})
+app.listen(3000, ()=>{
+    console.log("aplikacja działa");
+})
+})
